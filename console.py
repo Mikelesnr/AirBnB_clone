7604e357a88244e9b3b4c8a9c3c4a0698e8d77b2
AturiheihiBlendon <2020blendon@gmail.com>
2022-09-04 21:26:29 +0300

    Documentation

--- console.py
+++ console.py
@@ -8,12 +8,15 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
     def do_quit(self, args):
-        'quits the console'
+        'Quit command to exit the program'
         return True
 
     def do_EOF(self, args):
-        'EOF'
+        'Quit command to exit the program'
         return True
+
+    def emptyline(self):
+        return False
 
 
 console = HBNBCommand()
