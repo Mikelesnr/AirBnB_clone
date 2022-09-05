bb29dc382cdaf9d89ec15271293a978948984a5c
AturihehihiBlendon <2020blendon@gmail.com>
2022-09-05 04:03:50 +0300

    PEP8 fix

--- console.py
+++ console.py
@@ -14,15 +14,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
     def do_quit(self, args):
-        'Quit command to exit the program'
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, args):
-        'Quit command to exit the program'
+        """Quit command to exit the program"""
         return True
 
     def emptyline(self):
-        return False
+        """called when an empty line is entered"""
+        pass
 
 
 if __name__ == '__main__':
